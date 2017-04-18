b85d842ada31775ded89782593a0a48585a3f23e
Matias Leidemer <matiasleidemer@gmail.com>
2017-04-17 23:24:38 -0300

    Add missing logout route

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "spotify_callbacks" }
+  devise_for :users, controllers: { omniauth_callbacks: "spotify_callbacks" }
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
+  end
 
   root to: "home#index"
 
