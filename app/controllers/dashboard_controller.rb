3238536683d5c4d6100942be722ad35720471f9f
Matias Leidemer <matiasleidemer@gmail.com>
2017-04-20 01:19:19 -0300

    Display albums in the dashboard
    It is currently displaying all albums in the dashboard (we're not
    scoping it by the logged user, for instance). This is just to give a
    taste (and it tastes good).

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    @albums = Album.order('released_at DESC').includes(:artist).limit(9)
   end
 end
