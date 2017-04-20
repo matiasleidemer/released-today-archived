559230015fa52d8e7919e3c3eee7417e49b08f11
Matias Leidemer <matiasleidemer@gmail.com>
2017-04-20 01:09:36 -0300

    Add artist's singles to the mix
    Because the way the Spotify works, it is kinda uncertain we get the
    latest album (including single). This commit gets both the latest single
    and album, and persists them to the DB.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,5 +1,5 @@
 class Artist < ApplicationRecord
-  has_many :albums
+  has_many :albums, dependent: :destroy
 
   validates :name, :spotify_id, presence: true
 end
--- app/models/factories/album.rb
+++ app/models/factories/album.rb
@@ -1,15 +1,18 @@
 module Factories
   class Album
     def self.create_latest_from_artist(artist)
-      payload = RSpotify::Artist.find(artist.spotify_id).albums.sort_by(&:release_date).last
+      latest_album = RSpotify::Artist.find(artist.spotify_id).albums(album_type: 'album').sort_by(&:release_date).last
+      latest_single = RSpotify::Artist.find(artist.spotify_id).albums(album_type: 'single').sort_by(&:release_date).last
 
-      ::Album.find_or_create_by(spotify_id: payload.id) do |album|
-        album.artist = artist
-        album.spotify_id = payload.id
-        album.name = payload.name
-        album.image_url = payload.images.first["url"]
-        album.number_of_tracks = payload.tracks.size
-        album.released_at = payload.release_date
+      [latest_album, latest_single].each do |payload|
+        ::Album.find_or_create_by(spotify_id: payload.id) do |album|
+          album.artist_id = artist.id
+          album.spotify_id = payload.id
+          album.name = payload.name
+          album.image_url = payload.images.first["url"]
+          album.number_of_tracks = payload.tracks.size
+          album.released_at = payload.release_date
+        end
       end
     end
   end
