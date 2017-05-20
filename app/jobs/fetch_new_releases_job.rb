8ed5536304e7d4a482f29eabe82e74d1eab18f43
Matias Leidemer <matiasleidemer@gmail.com>
2017-05-20 10:40:52 -0300

    Timeout needs to be converted to integer...

--- app/jobs/fetch_new_releases_job.rb
+++ app/jobs/fetch_new_releases_job.rb
@@ -2,13 +2,13 @@ class FetchNewReleasesJob < ApplicationJob
   queue_as :default
 
   def perform
-    timeout = ENV["RETRY_TIMEOUT"]
+    timeout = ENV["RETRY_TIMEOUT"].to_i
 
     Spotify::ReleasesSyncer.call
   rescue RestClient::TooManyRequests
     puts "RestClient::TooManyRequests raised, waiting #{timeout} seconds..."
     sleep(timeout)
-    
+
     Spotify::ReleasesSyncer.call
   rescue Exception => error
     Bugsnag.notify(error)
