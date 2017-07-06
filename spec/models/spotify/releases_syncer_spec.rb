948911a930fe4287cf7a6bb1f653c15b518f829a
Matias Leidemer <matiasleidemer@gmail.com>
2017-07-05 21:45:54 -0300

    Update Spotify::ReleasesSyncer to not notify album releases
    Only albums released at the current day will generate user
    notifications.
    
    fixes #15

--- spec/models/spotify/releases_syncer_spec.rb
+++ spec/models/spotify/releases_syncer_spec.rb
@@ -5,8 +5,18 @@ RSpec.describe Spotify::ReleasesSyncer do
   let(:user)   { create(:user) }
 
   before do
+    # Only one album from the mock http call returns this date as its
+    # "release date". Instead of stubbing any instance of Album, we're
+    # mocking the current date, meaning one of the albums will be released
+    # today hence change the notification count from 0 to 1
+    Timecop.freeze(Time.zone.parse('2016-08-05'))
+
     artist.users << user
     artist.update_attribute(:updated_at, Time.zone.now - 2.days)
+  end
+
+  after do
+    Timecop.return
   end
 
   describe '.call' do
@@ -20,7 +30,7 @@ RSpec.describe Spotify::ReleasesSyncer do
     it "creates new release notifications for users that follow the artist" do
       VCR.use_cassette 'releases_syncer' do
         expect { described_class.call }
-          .to change { user.notifications.count }.from(0).to(2)
+          .to change { user.notifications.count }.from(0).to(1)
       end
     end
 
