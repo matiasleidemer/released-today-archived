6b0ad8ac571e5786f42429448923c800b5801355
Matias Henrique Leidemer <matiasleidemer@gmail.com>
2017-11-02 20:30:35 -0200

    Show user's emails frequency on admin page

--- app/dashboards/user_dashboard.rb
+++ app/dashboards/user_dashboard.rb
@@ -1,4 +1,4 @@
-require "administrate/base_dashboard"
+require 'administrate/base_dashboard'
 
 class UserDashboard < Administrate::BaseDashboard
   # ATTRIBUTE_TYPES
@@ -18,6 +18,7 @@ class UserDashboard < Administrate::BaseDashboard
     metadata: Field::String.with_options(searchable: false),
     created_at: Field::DateTime,
     updated_at: Field::DateTime,
+    email_frequency: Field::String.with_options(searchable: false)
   }.freeze
 
   # COLLECTION_ATTRIBUTES
@@ -25,41 +26,41 @@ class UserDashboard < Administrate::BaseDashboard
   #
   # By default, it's limited to four items to reduce clutter on index pages.
   # Feel free to add, remove, or rearrange items.
-  COLLECTION_ATTRIBUTES = [
-    :id,
-    :name,
-    :email,
-    :admin,
-    :uid,
-    :provider,
+  COLLECTION_ATTRIBUTES = %i[
+    id
+    name
+    email
+    uid
+    email_frequency
   ].freeze
 
   # SHOW_PAGE_ATTRIBUTES
   # an array of attributes that will be displayed on the model's show page.
-  SHOW_PAGE_ATTRIBUTES = [
-    :id,
-    :name,
-    :email,
-    :admin,
-    :provider,
-    :uid,
-    :token,
-    :metadata,
-    :created_at,
-    :updated_at,
+  SHOW_PAGE_ATTRIBUTES = %i[
+    id
+    name
+    email
+    admin
+    provider
+    uid
+    token
+    email_frequency
+    metadata
+    created_at
+    updated_at
   ].freeze
 
   # FORM_ATTRIBUTES
   # an array of attributes that will be displayed
   # on the model's form (`new` and `edit`) pages.
-  FORM_ATTRIBUTES = [
-    :name,
-    :email,
-    :admin,
-    :provider,
-    :uid,
-    :token,
-    :metadata
+  FORM_ATTRIBUTES = %i[
+    name
+    email
+    admin
+    provider
+    uid
+    token
+    metadata
   ].freeze
 
   # Overwrite this method to customize how users are displayed
