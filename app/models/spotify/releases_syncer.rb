6286f883cb46ffb1bd5b5283eb5e59818f7245e1
Matias Leidemer <matiasleidemer@gmail.com>
2019-01-19 20:47:31 -0200

    Update ReleasesSyncer to user Album instead of AlbumRepository

--- app/models/spotify/releases_syncer.rb
+++ app/models/spotify/releases_syncer.rb
@@ -2,18 +2,15 @@ module Spotify
   class ReleasesSyncer
     class << self
       def call(artists: nil, send_notification: true)
-        album_repository = Repositories::AlbumRepository.new
-
         artists ||= ::Artist.stale
 
         artists.find_each do |artist|
           spotify_artist = Spotify::Artist.new(artist.metadata)
 
           spotify_artist.latest_releases.each do |release|
-            next if album_repository.exists?(spotify_id: release.spotify_id)
+            next if ::Album.exists?(spotify_id: release.spotify_id)
 
-            attributes = release.attributes.merge(artist_id: artist.id)
-            album = album_repository.create(attributes)
+            album = ::Album.create_from_spotify(release.attributes, artist: artist)
 
             next unless send_notification
             next unless album.released_today?
