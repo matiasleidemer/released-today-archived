c21c670edb95c782fa02e460da3ce23f15abafa4
Matias Leidemer <matiasleidemer@gmail.com>
2019-01-19 20:45:06 -0200

    Move AlbumRepository.create to Album model

--- app/models/album.rb
+++ app/models/album.rb
@@ -5,6 +5,18 @@ class Album < ApplicationRecord
 
   validates :artist_id, :spotify_id, :name, :number_of_tracks, presence: true
 
+  def self.create_from_spotify(payload, artist:)
+    new(spotify_id: payload[:spotify_id]).tap do |album|
+      album.name = payload[:name]
+      album.image_url = payload[:image_url]
+      album.number_of_tracks = payload[:number_of_tracks]
+      album.released_at = payload[:released_at]
+      album.metadata = payload[:metadata]
+      album.artist = artist
+      album.save
+    end
+  end
+
   def followers
     artist.users
   end
--- spec/models/album_spec.rb
+++ spec/models/album_spec.rb
@@ -1,6 +1,32 @@
 require 'rails_helper'
 
 RSpec.describe Album do
+  describe '.create_from_spotify' do
+    let(:artist) { create(:artist) }
+    let(:payload) do
+      {
+        spotify_id: '1q2w3e',
+        name: 'Cool To Be You',
+        image_url: 'image.url',
+        number_of_tracks: 13,
+        released_at: Date.new(2012, 5, 8),
+        metadata: '{}'
+      }
+    end
+
+
+    it "creates a new album using the spotify payload and artist" do
+      album = described_class.create_from_spotify(payload, artist: artist)
+      expect(album.artist).to eql(artist)
+      expect(album.spotify_id).to eql('1q2w3e')
+      expect(album.name).to eql('Cool To Be You')
+      expect(album.image_url).to eql('image.url')
+      expect(album.number_of_tracks).to eql(13)
+      expect(album.released_at).to eql(Date.new(2012, 5, 8))
+      expect(album.metadata).to eql('{}')
+    end
+  end
+
   describe '#followers' do
     subject      { create(:album, artist: artist) }
     let(:artist) { create(:artist) }
