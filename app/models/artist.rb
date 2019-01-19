fb17131305ea29411ff629bca5cd03b0155a7245
Matias Leidemer <matiasleidemer@gmail.com>
2019-01-19 19:39:04 -0200

    Move ArtistRepository methods to Artist model

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -4,6 +4,18 @@ class Artist < ApplicationRecord
 
   validates :name, :spotify_id, presence: true
 
+  scope :stale, -> {
+    where("updated_at < ?", 22.hours.ago).where("disabled_at is null")
+  }
+
+  def self.create_or_update_from_spotify(data)
+    find_or_initialize_by(spotify_id: data[:spotify_id]).tap do |artist|
+      artist.name = data[:name]
+      artist.metadata = data[:metadata]
+      artist.save
+    end
+  end
+
   def image_url
     images = metadata.fetch("images", [])
     return if images.empty?
